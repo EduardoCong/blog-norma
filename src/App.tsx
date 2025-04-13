7da88fab6591cb01772e7ff9c85e50a51c6596bf
eduardocong <22090441@alumno.utmetropolitana.edu.mx>
2025-04-13 01:20:18 -0600

    fix: main

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Toaster } from 'react-hot-toast';
 import RoutesApp from './presentation/routes/routes';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster
+      position="top-center"
+      reverseOrder={false}
+      toastOptions={{
+        style: {
+          fontSize: "16px",
+          padding: "16px 24px",
+          borderRadius: "12px",
+          fontFamily: "Plus Jakarta Sans, sans-serif",
+          fontWeight: "800",
+        },
+      }}
+    ></Toaster>
       <RoutesApp />
     </BrowserRouter>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <Toaster
-      position="top-center"
-      reverseOrder={false}
-      toastOptions={{
-        style: {
-          fontSize: "16px",
-          padding: "16px 24px",
-          borderRadius: "12px",
-          fontFamily: "Plus Jakarta Sans, sans-serif",
-          fontWeight: "800",
-        },
-      }}
-    ></Toaster>
   </StrictMode>
 );
